71cb551195f44cb80fe988bcff496f6b98789640
clonn <clonncd@gmail.com>
2015-07-25 16:34:00 +0800

    update api spec

--- test/api.spec.js
+++ test/api.spec.js
@@ -5,9 +5,7 @@ var serNode = new SerNode({
   id: process.env["SER_ID"] || "",
   secret_key: process.env["SECRET_KEY"] || "",
 });
-console.log(serNode)
 before(function (done) {
-    console.log("before ----")
     serNode.getToken().then(function (data) {
       console.log(data);
       return done();
@@ -16,18 +14,13 @@ before(function (done) {
 
 describe("get some api run and test", function (done) {
   
-
   it("get ptt topic", function (done) {
-    console.log("ptt ----")
     serNode.request("top_article/ptt", {
       period: 10
     })
     .then(function (data) {
-      console.log("--- show result")
-      console.log(data);
       (data.message.indexOf("success") > -1).should.be.true;
       return done();
     });
   });
-
 });
